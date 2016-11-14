e11e639e649744d20faa1450f50a868573b06889
nonlux <nonluxi@gmail.com>
2016-11-15 01:00:09 +0800

    export redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export Root from './Root/Root';
 export DevTools from './DevTools/DevTools';
+export ReduxDevTools from './DevTools/ReduxDevTools';
 /* providers */
 export ReduxProvider from './Provider/ReduxProvider';
 
